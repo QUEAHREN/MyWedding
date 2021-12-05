04f019e0bf7a2c5ddf6cc3a85e3c38440305a23f
QUEAHREN <xsyooo@icloud.com>
2021-12-05 20:43:28 +0800

    Test Index Pages

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,12 +1,27 @@
 import { Component } from 'react'
 import { View, Text } from '@tarojs/components'
-import { AtButton } from 'taro-ui'
-
-import "taro-ui/dist/style/components/button.scss" // 按需引入
-
+import "taro-ui/dist/style/components/noticebar.scss";
+import "taro-ui/dist/style/components/button.scss" 
+import "taro-ui/dist/style/components/icon.scss";
+import "taro-ui/dist/style/components/badge.scss";
+import "taro-ui/dist/style/components/tab-bar.scss";
+import { AtButton,AtNoticebar,AtTabBar } from 'taro-ui'
 
 export default class Login extends Component {
 
+    constructor () {
+        super(...arguments)
+        this.state = {
+          current: 0
+        }
+      }
+
+    handleClick (value) {
+        this.setState({
+          current: value
+        })
+      }
+  
   componentWillMount () { }
 
   componentDidMount () { }
@@ -26,6 +41,18 @@ export default class Login extends Component {
         <AtButton type='primary' circle={true}>支持</AtButton>
         <Text>共建？</Text>
         <AtButton type='secondary' circle={true}>来</AtButton>
+        <AtNoticebar marquee>
+        这是 NoticeBar 通告栏，这是 NoticeBar 通告栏，这是 NoticeBar 通告栏
+        </AtNoticebar>
+        <AtTabBar
+  tabList={[
+    { title: '待办事项', iconType: 'bullet-list', text: 'new' },
+    { title: '拍照', iconType: 'camera' },
+    { title: '文件夹', iconType: 'folder', text: '100', max: 99 }
+  ]}
+  onClick={this.handleClick.bind(this)}
+  current={this.state.current}
+/>
       </View>
     )
   }
