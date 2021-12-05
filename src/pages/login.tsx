61b0415cbd9980fe0e25747d50912b891c842319
QUEAHREN <xsyooo@icloud.com>
2021-12-05 20:25:14 +0800

    Import taro-ui

---
+++ src/pages/login.tsx
@@ -0,0 +1,32 @@
+import { Component } from 'react'
+import { View, Text } from '@tarojs/components'
+import { AtButton } from 'taro-ui'
+
+import "taro-ui/dist/style/components/button.scss" // 按需引入
+
+
+export default class Login extends Component {
+
+  componentWillMount () { }
+
+  componentDidMount () { }
+
+  componentWillUnmount () { }
+
+  componentDidShow () { }
+
+  componentDidHide () { }
+
+  render () {
+    return (
+      <View className='index'>
+        <Text>Hello 123123!</Text>
+        <AtButton type='primary'>I need Taro UI</AtButton>
+        <Text>Taro UI 支持 Vue 了吗？</Text>
+        <AtButton type='primary' circle={true}>支持</AtButton>
+        <Text>共建？</Text>
+        <AtButton type='secondary' circle={true}>来</AtButton>
+      </View>
+    )
+  }
+}
