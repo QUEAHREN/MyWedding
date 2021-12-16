ed0ab24dd85c9a565083e5e9477d79be4199a5c6
QUEAHREN <xsyooo@icloud.com>
2021-12-16 16:39:15 +0800

    Success to upload image to COS

--- src/pages/Manage/NewWedding/NewWedding.tsx
+++ src/pages/Manage/NewWedding/NewWedding.tsx
@@ -2,75 +2,103 @@ import { Component } from 'react'
 import { Button, Text, Image, View, Map } from '@tarojs/components'
 import Taro from '@tarojs/taro'
 import './NewWedding.scss'
-import {  AtMessage } from "taro-ui"
+import { AtMessage } from "taro-ui"
 import { AtImagePicker } from 'taro-ui'
+import COS from 'cos-wx-sdk-v5'
 
 interface isState {
-  files:any
+    files: any
 }
+
+//临时处理方式
+var cos = new COS({
+    SecretId: 'AKIDiFWWFhsmPkob7MLVO9k7D6pPr7B1e189',
+    SecretKey: 'b2IJ4YlhMkrhGahfUAdroZ3Hew8D5kCP',
+});
+
 
 export default class NewWedding extends Component<any, isState> {
 
-    constructor () {
+    constructor() {
         super(...arguments)
         this.state = {
-          files: [{
-            url: 'https://jimczj.gitee.io/lazyrepay/aragaki1.jpeg',
-          },
-          {
-            url: 'https://jimczj.gitee.io/lazyrepay/aragaki2.jpeg',
-          },
-          {
-            url: 'https://jimczj.gitee.io/lazyrepay/aragaki3.png',
-          }]
+            files: []
         }
-      }
-      onChange (files) {
+    }
+
+    componentDidMount() {
+
+    }
+
+    onChange(files) {
         this.setState({
-          files
+            files
         })
-      }
-  render() {
-    return (
-        <view className="container">
-        <view className="page-body">
-
-            <AtImagePicker
-            files={this.state.files}
-            onChange={this.onChange.bind(this)}
-        />
+        console.log(this.state.files)
+    }
 
 
-          <form  catchreset="formReset">
-            <view className="page-section page-section-gap">
-              <view className="page-section-title">switch</view>
-              <switch name="switch"/>
-            </view>
-      
-        
-      
+    uploadInviting=()=>{
+        console.log(this.state.files[0].url)
+        cos.postObject({
+            Bucket: 'xusy-1300242514',
+            Region: 'ap-nanjing',
+            Key: 'WeddingInviting/' + 'testimg',
+            FilePath: this.state.files[0].url,
+            onProgress: function (info) {
+                console.log(JSON.stringify(info));
+            }
+        }, function (err, data) {
+            console.log("?")
+            console.log(err || data);
+        });
 
-      
-      
-            <view className="page-section">
-              <view className="page-section-title">input</view>
-              <view className="weui-cells weui-cells_after-title">
-                <view className="weui-cell weui-cell_input">
-                  <view className="weui-cell__bd" style="margin: 30rpx 0" >
-                    <input className="weui-input" name="input" placeholder="这是一个输入框" />
-                  </view>
+    }
+
+    render() {
+        return (
+            <view className="container">
+                <view className="page-body">
+
+                    <AtImagePicker
+                    
+                        length={1}
+                        files={this.state.files}
+                        onChange={this.onChange.bind(this)}
+                    />
+                    <Button onClick={this.uploadInviting}>上传图片</Button>
+
+
+                    <form catchreset="formReset">
+                        <view className="page-section page-section-gap">
+                            <view className="page-section-title">switch</view>
+                            <switch name="switch" />
+                        </view>
+
+
+
+
+
+
+                        <view className="page-section">
+                            <view className="page-section-title">input</view>
+                            <view className="weui-cells weui-cells_after-title">
+                                <view className="weui-cell weui-cell_input">
+                                    <view className="weui-cell__bd" style="margin: 30rpx 0" >
+                                        <input className="weui-input" name="input" placeholder="这是一个输入框" />
+                                    </view>
+                                </view>
+                            </view>
+                        </view>
+
+                        <view className="btn-area">
+                            <button style="margin: 30rpx 0" type="primary" formType="submit">Submit</button>
+                            <button style="margin: 30rpx 0" formType="reset">Reset</button>
+                        </view>
+                    </form>
                 </view>
-              </view>
+
             </view>
-      
-            <view className="btn-area">
-              <button style="margin: 30rpx 0" type="primary" formType="submit">Submit</button>
-              <button style="margin: 30rpx 0" formType="reset">Reset</button>
-            </view>
-          </form>
-        </view>
-      
-      </view>
-    )
-  }
+        )
+    }
 }
